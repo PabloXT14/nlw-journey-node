32318be0fa160c2dc8f50cad6fb4593965be4b1a
Pablo Alan <pabloxt14@gmail.com>
2024-07-09 00:42:45 -0300

    feat: config prisma orm

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,25 @@
 import fastify from 'fastify'
+import { prisma } from './lib/prisma'
 
 const app = fastify()
 const PORT = 3333
 
-app.get('/', async () => {
-  return 'Hello World'
+app.post('/register', async (req, reply) => {
+  await prisma.trip.create({
+    data: {
+      destination: 'Santa Catarina',
+      starts_at: new Date(),
+      ends_at: new Date(),
+    },
+  })
+
+  return reply.status(201).send({ message: 'Registro cadastrado com sucesso!' })
+})
+
+app.get('/list', async (req, reply) => {
+  const trips = await prisma.trip.findMany()
+
+  return reply.status(200).send({ trips })
 })
 
 app
