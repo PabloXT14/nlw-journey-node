fef560dbdca4f3b923fee2beb5ca29e8577c557e
Pablo Alan <pabloxt14@gmail.com>
2024-07-10 20:26:40 -0300

    feat: config CORS

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import fastify from 'fastify'
-import { createTrip } from './routes/create-trip'
 import {
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
+import cors from '@fastify/cors'
+
+import { createTrip } from './routes/create-trip'
 import { confirmTrip } from './routes/confirm-trip'
 
 const app = fastify()
 const PORT = 3333
+
+app.register(cors, {
+  origin: 'http://localhost:3000',
+})
 
 // Add schema validator and serializer
 app.setValidatorCompiler(validatorCompiler)
