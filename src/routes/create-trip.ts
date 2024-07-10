f90231d4bb2d4e6d6f99cd7d654a16a5e0b9c4fb
Pablo Alan <pabloxt14@gmail.com>
2024-07-09 23:00:33 -0300

    feat: send email to owner on create-trip

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -1,8 +1,11 @@
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import dayjs from 'dayjs'
+import nodemailer from 'nodemailer'
 import { z } from 'zod'
 import { prisma } from '../lib/prisma'
+import { getMailClient } from '../lib/mail'
+
 
 export async function createTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -13,11 +16,13 @@ export async function createTrip(app: FastifyInstance) {
           destination: z.string().min(4),
           starts_at: z.coerce.date(),
           ends_at: z.coerce.date(),
+          owner_name: z.string(),
+          owner_email: z.string().email(),
         }),
       },
     },
     async (req, reply) => {
-      const { destination, starts_at, ends_at } = req.body
+      const { destination, starts_at, ends_at, owner_name, owner_email } = req.body
 
       if (dayjs(starts_at).isBefore(new Date())) {
         throw new Error('Start date must be in the future')
@@ -34,6 +39,30 @@ export async function createTrip(app: FastifyInstance) {
           ends_at,
         },
       })
+
+      const mail = await getMailClient()
+
+      const message = await mail.sendMail({
+        from: {
+          name: 'Equipe plann.er',
+          address: 'oi@plann.er',
+        },
+        to: {
+          name: owner_name,
+          address: owner_email,
+        },
+        subject: 'Sua viagem no plann.er',
+        html: `
+          <p>Ola ${owner_name},</p>
+          <p>Voce acabou de criar uma nova viagem no plann.er.</p>
+          <p>Seu destino: ${destination}</p>
+          <p>Seu horário de partida: ${dayjs(starts_at).format('DD/MM/YYYY HH:mm')}</p>
+          <p>Seu horário de termino: ${dayjs(ends_at).format('DD/MM/YYYY HH:mm')}</p>
+          <p>Seja bem vindo ao plann.er!</p>
+        `,
+      })
+
+      console.log(nodemailer.getTestMessageUrl(message))
 
       return reply.send({ tripId: trip.id })
     },
