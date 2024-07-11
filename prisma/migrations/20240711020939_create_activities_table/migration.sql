d92f85ba68500c74c6734b95070a5bbc720d8d7d
Pablo Alan <pabloxt14@gmail.com>
2024-07-10 23:11:35 -0300

    feat: create activities table

---
+++ prisma/migrations/20240711020939_create_activities_table/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "activities" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "occurs_at" DATETIME NOT NULL,
+    "trip_id" TEXT NOT NULL,
+    CONSTRAINT "activities_trip_id_fkey" FOREIGN KEY ("trip_id") REFERENCES "trips" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
