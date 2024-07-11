00b1deac68016990adeeeaaa33f2e7aa6720e7c0
Pablo Alan <pabloxt14@gmail.com>
2024-07-11 00:32:59 -0300

    feat: create get-links route

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,7 @@ import { createActivity } from './routes/create-activity'
 import { createLink } from './routes/create-link'
 import { createTrip } from './routes/create-trip'
 import { getActivities } from './routes/get-activities'
+import { getLinks } from './routes/get-links'
 
 const app = fastify()
 const PORT = 3333
@@ -29,6 +30,7 @@ app.register(confirmParticipant)
 app.register(createActivity)
 app.register(getActivities)
 app.register(createLink)
+app.register(getLinks)
 
 app
   .listen({
