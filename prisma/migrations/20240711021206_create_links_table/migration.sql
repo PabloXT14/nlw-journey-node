6736079c028fa65ee0cb6359932a9e8d0ffb1ed2
Pablo Alan <pabloxt14@gmail.com>
2024-07-10 23:12:13 -0300

    feat: create links table

---
+++ prisma/migrations/20240711021206_create_links_table/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "links" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "trip_id" TEXT NOT NULL,
+    CONSTRAINT "links_trip_id_fkey" FOREIGN KEY ("trip_id") REFERENCES "trips" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
